587bd0fbe90a409ab8161efd4c36f3824b8ec201
tan.lin <tan.lin@linkingmed.com>
2018-07-12 18:39:15 +0800

    支持到手动配置刷新

--- springCloudConfig-client-8001/src/main/java/com/tan0/springboot/Application.java
+++ springCloudConfig-client-8001/src/main/java/com/tan0/springboot/Application.java
@@ -2,7 +2,9 @@ package com.tan0.springboot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 	
     public static void main( String[] args ){
--- springCloudConfig-server-8000/src/main/java/com/tan0/springboot/Application.java
+++ springCloudConfig-server-8000/src/main/java/com/tan0/springboot/Application.java
@@ -2,9 +2,11 @@ package com.tan0.springboot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class Application {
 	
     public static void main( String[] args ){
